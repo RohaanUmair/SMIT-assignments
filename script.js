0d2b429d1dbd702c4abb2afea1b5fe8c6ffc0453
RohaanUmair <m.rohaanumair@gmail.com>
2024-03-15 00:12:00 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let cityNames = ['Karachi', 'Lahore', 'Islamabad', 'Quetta', 'Peshawar'];
+let selectedCities = cityNames.slice(2, 4);
+
+console.log(selectedCities);
+
+
+
+
+let arr = ["This ", "is ", "my ", "cat"];
+let str = arr.join(" ");
+
+console.log(str);
