a0ee4041137d12c5a80b4828dceb04bc135c8734
RohaanUmair <m.rohaanumair@gmail.com>
2024-04-29 23:59:32 +0500

    Practice Questions Assignment

--- Assignment 9 - Practice Questions (functions)/main.js
+++ Assignment 9 - Practice Questions (functions)/main.js
@@ -30,3 +30,33 @@ function convertToNumber(str){
 
 console.log(convertToNumber('23'));
 console.log(convertToNumber('abcd'));
+
+
+
+
+// This function truncates a number to a specified number of decimals
+function truncateDecimal(num, decimals){
+    let output = (Math.round(num * (10 ** decimals))) / (10 ** decimals);
+    return output;
+}
+
+console.log(truncateDecimal(22.21234, 2));
+
+
+
+
+// This function formats a number to a string representation with the specified number of decimal places
+function formatNumber(num, decimalPlaces){
+    if (typeof num === 'NaN' || typeof num !== 'number'){
+        return 'Not a valid number';
+    }
+
+    if (typeof decimalPlaces === 'NaN' || typeof decimalPlaces !== 'number'){
+        return 'Invalid decimal places';
+    }
+
+    return num.toFixed(decimalPlaces);
+}
+
+console.log(formatNumber(2.123123, 2));
+console.log(formatNumber('12a', 2));
